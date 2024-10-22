3c9dc62af17d1e91ca32fadea8f49d1c7797048c
unknown <yliastupac25@gmail.com>
2024-10-22 13:29:18 +0300

    footer

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,9 @@
-  
+const btnBurger = document.querySelector('.btn-burger')
+const mobileContainer = document.querySelector('.mobile-container')
+
+const togleMobileMenu =()=> {
+	mobileContainer.classList.toggle('show')
+	btnBurger.classList.toggle('active')
+}
+
+btnBurger.addEventListener('click', togleMobileMenu)
