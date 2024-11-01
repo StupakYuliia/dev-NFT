cbc5d6b11957da59e9080449977087ca86a5e2c5
StupakYuliia <yliastupac25@gmail.com>
2024-11-01 17:18:03 +0200

    feat: section - banner done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -9,8 +9,10 @@ const togleMobileMenu =()=> {
 btnBurger.addEventListener('click', togleMobileMenu)
 
 const swiper = new Swiper('.swiper', {
+	slidesPerView: 2,
+	speed: 700,
 	loop: true,
-	spaceBetween: 20,
+	spaceBetween: 10,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
